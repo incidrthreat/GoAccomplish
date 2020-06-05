dc2ec676ef6c22cd1c81a6e1b00cebafe4887bb9
incidrthreat <incidrthreat@gmail.com>
2020-06-05 14:54:03 -0500

    Added a few user funcs

--- models/user.go
+++ models/user.go
@@ -15,7 +15,28 @@ type User struct {
 	Role	string `json:"role"`
 }
 
-// GetUser returns a user based on the ID
+// GetUser returns a user based on the ID, no user found system returns an error.
 func GetUser(id int64) (User, error) {
 	return nil, nil
+}
+
+// GetUsers returns an array of all registered users
+func GetUsers() ([]User, error) {
+	return nil, nil
+}
+
+// GetUserByUsername retreives data from db based on username
+func GetUserByUsername(username string) (User, error) {
+	return nil, nil
+}
+
+// UpdateUser updates user data
+func UpdateUser(user *User) error {
+	return nil
+}
+
+
+// DeleteUser deletes a user from the db
+func DeleteUser(id int64) error {
+	return nil
 }
